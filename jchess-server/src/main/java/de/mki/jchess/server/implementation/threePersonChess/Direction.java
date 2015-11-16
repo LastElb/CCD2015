53ac805a6335dd0adbb07a9ba9ba08a7356a1381
Igor Lückel <igor.lueckel@googlemail.com>
2015-11-16 18:23:50 +0100

    Added [NecessaryFreeDirectionsForDiagonal] to three person chess direction (to simplify diagonal movement)

--- jchess-server/src/main/java/de/mki/jchess/server/implementation/threePersonChess/Direction.java
+++ jchess-server/src/main/java/de/mki/jchess/server/implementation/threePersonChess/Direction.java
@@ -1,5 +1,9 @@
 package de.mki.jchess.server.implementation.threePersonChess;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by Igor on 13.11.2015.
  */
@@ -18,6 +22,7 @@ public enum Direction {
     TOPLEFT;
 
     private Direction opposite;
+    private List<Direction> freeForDiagonalMove;
 
     static {
         RIGHT.opposite = LEFT;
@@ -32,9 +37,21 @@ public enum Direction {
         DIAGONALTOPLEFT.opposite = DIAGONALBOTTOMRIGHT;
         BOTTOMRIGHT.opposite = TOPLEFT;
         TOPLEFT.opposite = BOTTOMRIGHT;
+
+        DIAGONALTOP.freeForDiagonalMove = Arrays.asList(TOPLEFT, TOPRIGHT);
+        DIAGONALBOTTOM.freeForDiagonalMove = Arrays.asList(BOTTOMRIGHT, BOTTOMLEFT);
+        DIAGONALTOPRIGHT.freeForDiagonalMove = Arrays.asList(TOPRIGHT, RIGHT);
+        DIAGONALBOTTOMRIGHT.freeForDiagonalMove = Arrays.asList(RIGHT, BOTTOMRIGHT);
+        DIAGONALBOTTOMLEFT.freeForDiagonalMove = Arrays.asList(BOTTOMLEFT, LEFT);
+        DIAGONALTOPLEFT.freeForDiagonalMove = Arrays.asList(LEFT, TOPLEFT);
     }
 
     public Direction getOppositeDirection() {
         return opposite;
     }
+
+    public Optional<List<Direction>> getNecessaryFreeDirectionsForDiagonal() {
+        return Optional.ofNullable(freeForDiagonalMove);
+    }
+
 }
