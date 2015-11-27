fddee4e7d27905c73aa0421c600541166ea7fff8
Igor Lückel <igor.lueckel@googlemail.com>
2015-11-27 17:51:32 +0100

    Added REST API to client

---
+++ jchess-client/src/main/java/jchess/client/models/HistoryEntry.java
@@ -0,0 +1,30 @@
+package jchess.client.models;
+
+import java.util.List;
+
+/**
+ * Needs some thinking process
+ * Created by Igor on 13.11.2015.
+ */
+public class HistoryEntry {
+    List<ChessboardEvent> chessboardEvents;
+    Client player;
+
+    public List<ChessboardEvent> getChessboardEvents() {
+        return chessboardEvents;
+    }
+
+    public HistoryEntry setChessboardEvents(List<ChessboardEvent> chessboardEvents) {
+        this.chessboardEvents = chessboardEvents;
+        return this;
+    }
+
+    public Client getPlayer() {
+        return player;
+    }
+
+    public HistoryEntry setPlayer(Client player) {
+        this.player = player;
+        return this;
+    }
+}
