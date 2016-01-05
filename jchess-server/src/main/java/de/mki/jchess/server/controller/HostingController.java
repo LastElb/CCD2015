ab59badb020b902f721983d18cdd875c9226eabe
Malte Müns <m.muens@muensmedia.de>
2016-01-05 11:43:18 +0100

    [Server] Shorten gameID

--- jchess-server/src/main/java/de/mki/jchess/server/controller/HostingController.java
+++ jchess-server/src/main/java/de/mki/jchess/server/controller/HostingController.java
@@ -33,7 +33,7 @@ public class HostingController {
         Game game;
         switch (name) {
             case "default-3-person-chess":
-                game = new ThreePersonGame(RandomStringService.getRandomString());
+                game = new ThreePersonGame(RandomStringService.getRandomString(4));
                 break;
             default:
                 game = null;
