ea0118bff61e30bf00040383750f82ea86ab8738
Kevin Lamshöft <Gheek@users.noreply.github.com>
2016-01-12 12:57:34 +0100

    [Hexboard] cleanup

--- jchess-client/src/main/java/de/mki/chessboard/implementation/threePersonChess/Layout.java
+++ jchess-client/src/main/java/de/mki/chessboard/implementation/threePersonChess/Layout.java
@@ -3,37 +3,28 @@ package de.mki.chessboard.implementation.threePersonChess;
 import java.awt.*;
 
 /**
- * Created by Kevin on 24.11.15.
+ * Class with represents the layout of a hexagonal chessboard.
  */
 public class Layout {
 
     Orientation orientation;
-    Point size;
-    int IntSize;
     double sizeX;
     double sizeY;
     Point origin;
     public static final Orientation pointy = new Orientation(Math.sqrt(3.0), Math.sqrt(3.0) / 2.0, 0.0, 3.0 / 2.0, Math.sqrt(3.0) / 3.0, -1.0 / 3.0, 0.0, 2.0 / 3.0, 0.5);
     public static final Orientation flat = new Orientation(3.0 / 2.0, 0.0, Math.sqrt(3.0) / 2.0, Math.sqrt(3.0), 2.0 / 3.0, 0.0, -1.0 / 3.0, Math.sqrt(3.0) / 3.0, 0.0);
 
-    public Layout(Orientation orientation, Point size, double sizeX, double sizeY, Point origin)
+    public Layout(Orientation orientation, double sizeX, double sizeY, Point origin)
     {
         this.orientation = orientation;
-        this.size = size;
         this.sizeX = sizeX;
         this.sizeY = sizeY;
         this.origin = origin;
     }
 
-    public Layout(Orientation orientation, int size, Point origin) {
-        this.orientation = orientation;
-        this.IntSize = size;
-        this.origin = origin;
-    }
 
     public Point hexagonToPixel(Hexagon h) {
         Orientation M = this.orientation;
-        Point size = this.size;
         double sizeX = this.sizeX;
         double sizeY = this.sizeY;
         Point origin = this.origin;
@@ -43,41 +34,21 @@ public class Layout {
     }
 
 
-    /*
-        public Point hexagonToPixel(Hexagon h) {
-            int size = this.IntSize;
-            Point origin = this.origin;
-            double x = size * Math.sqrt(3) * (h.q + h.r / 2);
-            double y = size * 3/2 * h.r;
-            return new Point((int) x + origin.x, (int) y + origin.y);
-        }
-    */
     public Hexagon pixelToHexagon(Point p)
     {
         Orientation M = this.orientation;
-        Point size = this.size;
         Point origin = this.origin;
-        Point pt = new Point((p.x - origin.x) / size.x, (p.y - origin.y) / size.y);
-        double q = M.b0 * pt.x + M.b1 * pt.y;
-        double r = M.b2 * pt.x + M.b3 * pt.y;
+        double sizeX = this.sizeX;
+        double sizeY = this.sizeY;
+
+        double diffX = (p.x - origin.x) / sizeX;
+        double diffY = (p.y - origin.y) / sizeY;
+        double q = M.b0 * diffX + M.b1 * diffY;
+        double r = M.b2 * diffX + M.b3 * diffY;
         double s = -q -r;
-        System.out.println("Seems to be q=" + q + " and r=" + r);
         return roundToNextHexagon(q,r,s);
     }
 
-    /*
-        public Hexagon pixelToHexagon(Point p) {
-            int size = this.IntSize;
-            Point origin = this.origin;
-            int x = p.x - origin.x;
-            int y = p.y - origin.y;
-            double q = (x * Math.sqrt(3)/3 -y/3) / size;
-            double r = y * 2/3 / size;
-            double s = -q -r;
-            System.out.println("Seems to be q=" + q + " and r=" +r);
-            return roundToNextHexagon(q,r,s);
-        }
-    */
     private Hexagon roundToNextHexagon(double q_, double r_, double s_) {
 
         int q = (int)(Math.round(q_));
--- jchess-client/src/main/java/de/mki/chessboard/implementation/threePersonChess/smallHexboard.java
+++ jchess-client/src/main/java/de/mki/chessboard/implementation/threePersonChess/smallHexboard.java
@@ -19,7 +19,7 @@ public class smallHexboard extends Hexboard {
         int vertDistance = 39; //52*3/4;
         this.setHexSize(new Point(26, 26));  //TODO: find fitting size.
         this.setOrigin(new Point(138, 65));     //TODO: find the actual origin point in the gui.
-        this.setLayout(new Layout(Layout.pointy, new Point(26, 26), 26, 26.5, new Point(138, 65)));
+        this.setLayout(new Layout(Layout.pointy, 26, 26.5, new Point(138, 65)));
         this.setImage(loadImage("chessboard600x600.png"));
     }
 }
