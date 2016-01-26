b6b7c3b177c5efd32d513795f888246fe6ed572b
Malte Müns <m.muens@muensmedia.de>
2016-01-26 14:28:13 +0100

    [Client] Improve JavaDocs in ServerAPI

--- jchess-client/src/main/java/jchess/client/ServerApi.java
+++ jchess-client/src/main/java/jchess/client/ServerApi.java
@@ -8,16 +8,13 @@ import de.mki.jchess.commons.Client;
 import jchess.client.models.Game;
 import jchess.client.models.Position;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 /**
- * Created by Igor on 27.11.2015.
+ * API to communicate with the server and map JSON to objects
  */
 public class ServerApi {
 
@@ -29,19 +26,30 @@ public class ServerApi {
         this.port = port;
     }
 
+    /**
+     * Get the hostname of the server
+     *
+     * @return hostname
+     */
     public String getHost() {
         return host;
     }
 
+    /**
+     * Get the port of the server
+     *
+     * @return port
+     */
     public int getPort() {
         return port;
     }
 
     /**
+     * Connect to a hosted game.
      *
-     * @param nickname
-     * @param gameId
-     * @return
+     * @param nickname Nickname of the new player
+     * @param gameId   Identifier of the game to join
+     * @return Client Client-Model with the Client-Identifier
      * @throws Exception
      */
     public Optional<Client> connectToGame(String nickname, String gameId) throws Exception {
@@ -63,8 +71,9 @@ public class ServerApi {
     }
 
     /**
+     * Get the supported game Modes from the server
      *
-     * @return
+     * @return List of Game Modes (Strings)
      * @throws Exception
      */
     public List<String> getAvailableGameModes() throws Exception {
@@ -74,16 +83,18 @@ public class ServerApi {
         Response response = future.get();
         asyncHttpClient.close();
         if (response.getStatusCode() == 200) {
-            return objectMapper.readValue(response.getResponseBody(), new TypeReference<List<String>>() {});
+            return objectMapper.readValue(response.getResponseBody(), new TypeReference<List<String>>() {
+            });
         } else {
             throw new Exception(response.getResponseBody());
         }
     }
 
     /**
-     * 
-     * @param gameMode
-     * @return
+     * Request a new hosted game on the server
+     *
+     * @param gameMode Mode of the game e.g. default-3-person-chess
+     * @return Game Model of the started Game containing the Identifier of the game
      * @throws Exception
      */
     public Optional<Game> hostGame(String gameMode) throws Exception {
@@ -101,8 +112,9 @@ public class ServerApi {
 
     /**
      * Get all available game information from the server
-     * @param gameID
-     * @return
+     *
+     * @param gameID Identifier of the game
+     * @return Game Model of the started Game containing the Identifier of the game
      * @throws Exception
      */
     public Optional<Game> getFullGame(String gameID) throws Exception {
@@ -119,6 +131,14 @@ public class ServerApi {
     }
 
 
+    /**
+     * Request the possible moves for a figure on specified field
+     *
+     * @param gameID        Identifier of the game
+     * @param fieldNotation Field the figure is located e.g. A4
+     * @return ArrayList<String> Array Containing all possible Fields to move to as {@link Position} object
+     * @throws Exception
+     */
     public ArrayList<String> getPossibleMoves(String gameID, String fieldNotation) throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
@@ -126,11 +146,11 @@ public class ServerApi {
         Response response = future.get();
         asyncHttpClient.close();
         if (response.getStatusCode() == 200) {
-            List<Position> positions =  objectMapper.readValue(response.getResponseBody(), objectMapper.getTypeFactory().constructCollectionType(List.class, Position.class));
+            List<Position> positions = objectMapper.readValue(response.getResponseBody(), objectMapper.getTypeFactory().constructCollectionType(List.class, Position.class));
 
             // cast position to List of Notations
             ArrayList<String> fieldsToHighlight = new ArrayList<String>();
-            for (Position position  : positions) {
+            for (Position position : positions) {
                 fieldsToHighlight.add(position.getNotation());
             }
 
@@ -140,6 +160,15 @@ public class ServerApi {
         }
     }
 
+    /**
+     * Try to perform a movement form source field to target field.
+     *
+     * @param gameID              Identifier of the game
+     * @param sourcefieldNotation Field the figure is located e.g. A4
+     * @param targetFieldNotation Field the figure should be moved to
+     * @param clientID            Identifier of the Client requesting this movement
+     * @throws Exception
+     */
     public void performMovement(String gameID, String sourcefieldNotation, String targetFieldNotation, String clientID) throws Exception {
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
         Future<Response> future = asyncHttpClient.preparePost("http://" + host + ":" + port + "/game/" + gameID + "/performMoveByField/" + sourcefieldNotation.toLowerCase() + "/" + clientID + "/" + targetFieldNotation.toLowerCase()).execute();
