38a3732d7442da6faa471d201204dcd3cee0bf66
Malte Müns <m.muens@muensmedia.de>
2016-01-26 15:13:15 +0100

    [Client] ADD JavaDoc description for packages

---
+++ jchess-client/src/main/java/jchess/client/models/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the models to map JSON to objects
+ */
+package jchess.client.models;
---
+++ jchess-client/src/main/java/jchess/client/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the java client for the chess game
+ */
+package jchess.client;
